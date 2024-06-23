d6374b337ce7d190a2e9c2049d541c68eae755d2
Siskodemon <whiteprist@gmail.com>
2024-06-23 18:18:41 +0300

    It's Begin

---
+++ AOP_Spring_Testing/src/main/java/com/evilcorp/AopSpringTestingApplication.java
@@ -0,0 +1,13 @@
+package com.evilcorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopSpringTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopSpringTestingApplication.class, args);
+	}
+
+}
